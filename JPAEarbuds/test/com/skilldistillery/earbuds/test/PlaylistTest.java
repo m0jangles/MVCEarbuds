7d87af6ad1872b1b0382cae89d71067788c4363b
Amber Moreno <amber.e.moreno@gmail.com>
2018-08-31 12:30:30 -0600

    profile, playlist passing tests

--- JPAEarbuds/test/com/skilldistillery/earbuds/test/PlaylistTest.java
+++ JPAEarbuds/test/com/skilldistillery/earbuds/test/PlaylistTest.java
@@ -1,6 +1,8 @@
 package com.skilldistillery.earbuds.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.skilldistillery.earbuds.entities.Playlist;
+import com.skilldistillery.earbuds.entities.Song;
 
 class PlaylistTest {
 	
@@ -25,17 +28,38 @@ class PlaylistTest {
 		playlist = em.find(Playlist.class, 1);
 	}
 	
-//	@Test
-//	void test_relationship_song() {
-//		assertEquals("", playlist.getSongs().get(0).getTitle());
-//	
-//	}
-//	
-//	@Test
-//	void test_relationship_profile() {
-//		assertEquals()
-//	
-//	}
+	@Test
+	void test_playlist_mappings() {
+		assertEquals("Songs to Sing in the Shower", playlist.getPlaylistName());
+	}
+	
+	@Test
+	void test_relationship_song() {
+		assertEquals("Africa", playlist.getSongs().get(0).getTitle());
+	
+	}
+	
+	@Test
+	void test_relationship_profile() {
+		assertEquals("Jason", playlist.getProfile().getFirstName());
+	
+	}
+	
+	@Test
+	void test_playlist_addSong_adds_song() {
+		Song song = new Song();
+		song.setTitle("Smoosh");
+		playlist.addSong(song);
+		assertTrue(playlist.getSongs().contains(song));
+	}
+	
+	@Test
+	void test_playlist_removeSong_removes_song() {
+		Song song = em.find(Song.class, 1);
+		playlist.removeSong(song);
+		assertFalse(playlist.getSongs().contains(song));
+	}
+	
 
 	@AfterEach
 	void tearDown() throws Exception {
