c0fb01788fc25af44224210ea44c359f457ea704
Samantha Janik <samajanik@gmail.com>
2018-08-31 09:58:26 -0600

    stubbed out User and Song classes

--- JPAEarbuds/src/com/skilldistillery/earbuds/entities/Song.java
+++ JPAEarbuds/src/com/skilldistillery/earbuds/entities/Song.java
@@ -1,5 +1,117 @@
 package com.skilldistillery.earbuds.entities;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Song {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String album;
+	@Column(name = "album_image")
+	private String albumImage;
+	private String url;
+	private String artist;
+	@ManyToMany
+	@JoinTable(name = "playlist_song", joinColumns = @JoinColumn(name = "song_id"), inverseJoinColumns = @JoinColumn(name = "playlist_id"))
+	private List<Playlist> playlists;
+	@ManyToMany(mappedBy = "songs")
+	private List<Genre> genres;
+
+	public Song() {
+	}
+
+	public Song(int id, String title, String album, String albumImage, String url, String artist) {
+		this.id = id;
+		this.title = title;
+		this.album = album;
+		this.albumImage = albumImage;
+		this.url = url;
+		this.artist = artist;
+	}
+
+	// Getters and Setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getAlbumImage() {
+		return albumImage;
+	}
+
+	public void setAlbumImage(String albumImage) {
+		this.albumImage = albumImage;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public List<Playlist> getPlaylists() {
+		return playlists;
+	}
+
+	public void setPlaylists(List<Playlist> playlists) {
+		this.playlists = playlists;
+	}
+
+	public List<Genre> getGenres() {
+		return genres;
+	}
+
+	public void setGenres(List<Genre> genres) {
+		this.genres = genres;
+	}
+
+	// Add and Remove Helpers
+
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", title=" + title + ", album=" + album + ", albumImage=" + albumImage + ", url="
+				+ url + ", artist=" + artist + "]";
+	}
 
 }
--- JPAEarbuds/src/com/skilldistillery/earbuds/entities/User.java
+++ JPAEarbuds/src/com/skilldistillery/earbuds/entities/User.java
@@ -1,6 +1,95 @@
 package com.skilldistillery.earbuds.entities;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+@Entity
 public class User {
-	// this is a test from sam
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	@OneToOne(mappedBy = "user")
+	private Profile profile;
+	@ManyToMany
+	@JoinTable(name = "user_friend", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "friend_id"))
+	private List<User> friends;
+
+	public User(int id, String username, String password, String email) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
+	public User() {
+	}
+
+	// Getters and Setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+	public List<User> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(List<User> friends) {
+		this.friends = friends;
+	}
+	
+	// Add and Remove Helpers
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + "]";
+	}
 
 }
