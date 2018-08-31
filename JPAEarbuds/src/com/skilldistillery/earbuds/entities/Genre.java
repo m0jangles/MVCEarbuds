4985d22b1182af25168441e9f81b0a1a7c155260
Richard Paleczny <palecznyr@gmail.com>
2018-08-31 09:59:20 -0600

    Created initial Genre and Location entities. Updated persistence.xml file to include initial entities for <class> tags

--- JPAEarbuds/src/com/skilldistillery/earbuds/entities/Genre.java
+++ JPAEarbuds/src/com/skilldistillery/earbuds/entities/Genre.java
@@ -1,5 +1,118 @@
 package com.skilldistillery.earbuds.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Genre {
+
+	// fields
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	@ManyToMany
+	@JoinTable(name = "song_genre", joinColumns = @JoinColumn(name = "genre_id"),
+			inverseJoinColumns = @JoinColumn(name = "song_id"))
+	private List<Song> songs;
+
+	// constructors
+
+	public Genre() {
+	}
+
+	public Genre(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	// getters & setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Song> getSongs() {
+		return songs;
+	}
+
+	public void setSongs(List<Song> songs) {
+		this.songs = songs;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// helpers
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Genre other = (Genre) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Genre [id=" + id + ", name=" + name + "]";
+	}
+
+	public void addSong(Song song) {
+		if (songs == null)
+			songs = new ArrayList<>();
+
+		if (!songs.contains(song)) {
+			song.addGenre(this);
+		}
+	}
+
+	public void removeSong(Song song) {
+		if (songs != null && songs.contains(song)) {
+			songs.remove(song);
+			song.removeGenre(this);
+		}
+	}
 
 }
