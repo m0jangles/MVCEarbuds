f930f16afd16af89c355363d2ad639b1b0118e01
Amber Moreno <amber.e.moreno@gmail.com>
2018-08-31 14:55:54 -0600

    AuthenticationDAOImpl methods added

--- MVCEarbuds/src/main/java/com/skilldistillery/earbuds/data/AuthenticationDAO.java
+++ MVCEarbuds/src/main/java/com/skilldistillery/earbuds/data/AuthenticationDAO.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.earbuds.data;
 
+import com.skilldistillery.earbuds.entities.User;
+
 public interface AuthenticationDAO {
+
+	public User getUserByUserNameAndPassword(String userName, String password);
+
+	public User createUser(User user);
+
+	public User findUserById(int userId);
+
+	public boolean destroyUser(User user);
 
 }
