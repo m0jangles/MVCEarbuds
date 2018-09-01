3e4730916c8ac8e0d4cada43291ae40b992dd8d6
Amber Moreno <amber.e.moreno@gmail.com>
2018-09-01 16:48:02 -0600

    methods for displaying playlists

--- MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/PlaylistController.java
+++ MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/PlaylistController.java
@@ -1,11 +1,16 @@
 package com.skilldistillery.earbuds.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.skilldistillery.earbuds.data.PlaylistDAO;
+import com.skilldistillery.earbuds.entities.Playlist;
+import com.skilldistillery.earbuds.entities.User;
 
 @Controller
 public class PlaylistController {
@@ -14,7 +19,11 @@ public class PlaylistController {
 	private PlaylistDAO dao;
 
 	@RequestMapping(path = "viewPlaylists.do", method = RequestMethod.GET)
-	public String viewPlaylists() {
+	public String viewPlaylists(Model model, User user) {
+		List<Playlist> list= dao.getPlaylistsByUser(user);
+		model.addAttribute(list);
 		return "viewPlaylists";
 	}
+	
+
 }
