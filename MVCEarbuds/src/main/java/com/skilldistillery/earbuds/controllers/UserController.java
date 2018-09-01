f892fcef979b84b9599481334b1a8ae1f99f561e
Richard Paleczny <palecznyr@gmail.com>
2018-09-01 15:34:22 -0600

    Added first name and last name fields for sign up form

--- MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/UserController.java
+++ MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.earbuds.data.UserDAO;
@@ -22,7 +23,9 @@ public class UserController {
 
 	@RequestMapping(path = "signUp.do", method = RequestMethod.POST)
 	public String createNewUser(@ModelAttribute("userSignUp") @Valid User newUser,
-			BindingResult result, Model model, RedirectAttributes redir) {
+			BindingResult result, Model model, RedirectAttributes redir,
+			@RequestParam("firstName") String firstName,
+			@RequestParam("lastName") String lastName) {
 
 		if (result.hasErrors()) {
 			model.addAttribute("user", new User());
