d3738632b4fc8a6680ec3ab13497eb0cc146e592
Samantha Janik <samajanik@gmail.com>
2018-09-04 10:48:33 -0600

    added playlist name to model for songs list

--- MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/PlaylistController.java
+++ MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/PlaylistController.java
@@ -34,9 +34,11 @@ public class PlaylistController {
 	public String getSongs(Model model, @RequestParam("id") Integer playlistId) {
 
 		List<Song> songs = dao.getSongs(playlistId);
+		String playlistName = dao.getPlaylistById(playlistId).getPlaylistName();
 		if (songs != null) {
 			model.addAttribute("songs", songs);
 		}
+		model.addAttribute("playlistName", playlistName);
 
 		return "viewPlaylists";
 	}
