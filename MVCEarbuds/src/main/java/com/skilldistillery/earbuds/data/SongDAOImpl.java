43c71a5309a5624d828dc82f8d0c351fd55949f6
Amber Moreno <amber.e.moreno@gmail.com>
2018-09-03 18:36:30 -0600

    syncing up

--- MVCEarbuds/src/main/java/com/skilldistillery/earbuds/data/SongDAOImpl.java
+++ MVCEarbuds/src/main/java/com/skilldistillery/earbuds/data/SongDAOImpl.java
@@ -1,7 +1,29 @@
 package com.skilldistillery.earbuds.data;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
+import com.skilldistillery.earbuds.entities.Playlist;
+import com.skilldistillery.earbuds.entities.Song;
+
+@Component
+@Transactional
 public class SongDAOImpl {
 	
+	@PersistenceContext
+	private EntityManager em;
+	
+	public Song addSongToPlaylist(Integer id, Song song, String title, String album, String url, String artist) {
+		Playlist playlist = em.find(Playlist.class, id);
+		playlist.addSong(song);
+		song.setTitle(title);
+		
+		return song;
+		
+	}
 	
 
 }
