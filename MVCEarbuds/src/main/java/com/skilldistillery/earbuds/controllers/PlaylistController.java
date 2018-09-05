9d0bb2007712edc94b64c7b4162abde0ce230d59
Richard Paleczny <palecznyr@gmail.com>
2018-09-04 18:13:52 -0600

    Avoid merge conflict w/ useless changes

--- MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/PlaylistController.java
+++ MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/PlaylistController.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.earbuds.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.earbuds.data.PlaylistDAO;
+import com.skilldistillery.earbuds.entities.Genre;
 import com.skilldistillery.earbuds.entities.Playlist;
 import com.skilldistillery.earbuds.entities.Song;
 
@@ -21,7 +23,8 @@ public class PlaylistController {
 	@Autowired
 	private PlaylistDAO dao;
 
-	@RequestMapping(path = "viewPlaylists.do", params = "id", method = RequestMethod.GET)
+	@RequestMapping(path = "viewPlaylists.do", params = "id",
+			method = RequestMethod.GET)
 	public String viewPlaylists(Model model, Integer id) {
 		List<Playlist> playlists = dao.getPlaylists(id);
 		if (!playlists.isEmpty()) {
@@ -30,10 +33,27 @@ public class PlaylistController {
 		return "homepage";
 	}
 
-	@RequestMapping(path = "getSongs.do", params = "id", method = RequestMethod.GET)
+	@RequestMapping(path = "getSongs.do", params = "id",
+			method = RequestMethod.GET)
 	public String getSongs(Model model, @RequestParam("id") Integer playlistId) {
 
 		List<Song> songs = dao.getSongs(playlistId);
+
+		// ** NEW STUFF ** TODO
+		// Get the list of genres for every song in the song list
+		
+		
+		List<Genre> genreList = songs.get(0).getGenres();
+		
+		List<List<Genre>> genreListofLists = new ArrayList<>();
+		for (Song song : songs) {
+			genreListofLists.add(song.getGenres());
+		}
+
+		model.addAttribute("genreList", genreListofLists);
+		
+		// ** END NEW STUFF **
+
 		String playlistName = dao.getPlaylistById(playlistId).getPlaylistName();
 		if (songs != null) {
 			model.addAttribute("songs", songs);
@@ -66,12 +86,14 @@ public class PlaylistController {
 			return mv;
 		}
 	}
-	
+
 	@RequestMapping(path = "removePlaylist.do", method = RequestMethod.POST)
 	public ModelAndView deletePlaylist(Integer playlistId) {
 		ModelAndView mv = new ModelAndView();
 		Boolean deletedPlaylist = dao.removePlaylist(playlistId);
-		mv.addObject("deletedPlaylist", deletedPlaylist); // To be able to display a message when the playlist is deleted
+		mv.addObject("deletedPlaylist", deletedPlaylist); // To be able to display a
+																											// message when the
+																											// playlist is deleted
 		mv.setViewName("homepage");
 		return mv;
 	}
