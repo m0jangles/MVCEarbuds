5f4081c1d6d551e42e8e2d1121deec119a2304a4
Samantha Janik <samajanik@gmail.com>
2018-09-05 18:37:36 -0600

    changed routing in viewAnotherUserProfile

--- MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/ProfileController.java
+++ MVCEarbuds/src/main/java/com/skilldistillery/earbuds/controllers/ProfileController.java
@@ -93,7 +93,7 @@ public class ProfileController {
 		return "redirect:homepage.do";	
 	}
 	
-	@RequestMapping(path = "viewAnotherProfile.do", method = RequestMethod.GET)
+	@RequestMapping(path = "friendProfile.do", method = RequestMethod.GET)
 	public String viewAnotherUserProfile(Model model, Integer friendId) {
 		Profile otherProfile = userdao.getUserProfileById(friendId);
 		if (otherProfile != null) {
